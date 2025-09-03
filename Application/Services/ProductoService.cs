c847e426efbbfb3f00bd7046c8aab32b9f5d6bda
Agustin Baez <agustinramirobaez@hotmail.com>
2025-09-02 22:37:08 -0300

    add CreateProductoRequest validations

--- Application/Services/ProductoService.cs
+++ Application/Services/ProductoService.cs
@@ -36,11 +36,6 @@ namespace Application.Services
         {
             var productos = _productoRepository.GetAll();
 
-            if (string.IsNullOrEmpty(producto.Nombre) || producto.Precio <= 0 || producto.Stock < 0)
-            {
-                return false;
-            }
-
             producto.Id = productos.Any() ? productos.Max(x => x.Id) + 1 : 1;
 
             producto.Stock = producto.Stock == null ? 10 : producto.Stock.Value;
--- Contracts/Requests/CreateProductoRequest.cs
+++ Contracts/Requests/CreateProductoRequest.cs
@@ -1,10 +1,19 @@
-﻿namespace Contracts.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Contracts.Requests
 {
     public class CreateProductoRequest
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
         public string Nombre { get; set; } = string.Empty;
+
+        [Required]
+        [Range(1, (double)decimal.MaxValue, ErrorMessage = "El rango debe ser mayor a 0")]
         public decimal Precio { get; set; }
+
+        [Range(0, int.MaxValue)]
         public int? Stock { get; set; }
     }
 }
